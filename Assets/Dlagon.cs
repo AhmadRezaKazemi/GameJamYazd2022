9d959aef526ef268b86b8f8939ccac788dfdf650
RezaMC1999 <rezamc1999@yahoo.com>
2022-10-12 15:32:41 +0330

    btfh
    htfhtfh

---
+++ Assets/Dlagon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dlagon : MonoBehaviour
+{
+    public Animator animator;
+    void Start()
+    {
+        StartCoroutine(ENNN());   
+    }
+
+private IEnumerator ENNN()
+    {
+        yield return new WaitForSeconds(3f);
+        animator.enabled = true;
+    }
+}
